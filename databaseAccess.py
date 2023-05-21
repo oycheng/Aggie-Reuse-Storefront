f58eec13ae5eb1b6c919acfd8d8b322cd58df774
oycheng <123531322+oycheng@users.noreply.github.com>
2023-05-20 19:07:53 -0700

    added remove item and unreserve functions

--- databaseAccess.py
+++ databaseAccess.py
@@ -1,6 +1,5 @@
 
 import numpy as np
-import re
 import pandas as pd
 import sqlite3 as sql
 
@@ -17,19 +16,22 @@ class Access:
         except:
             self.dataDf = pd.DataFrame()
         dbConn.close()
-    def _duplicateCheck(self, data):
+        
+    def _dupCheck(self, barcode):
         if self.dataDf.empty:
-            return True
+            return
         database = self.dataDf.to_dict()
         barcodes = list(database["barcode"].values())
-        if data["barcode"][0] in barcodes:
-            return False
-        else:
+        if barcode in barcodes:
             return True
-    def store(self, data, location):
+        else:
+            return False
+    def store(self, barcode, location):
         self._extract(location)
-        inDataDf = pd.DataFrame(data)
-        if(self._duplicateCheck(data)):
+        if(not self._dupCheck(barcode)):
+            data = {"barcode" : [barcode],
+                    "reserved": ["false"]}
+            inDataDf = pd.DataFrame(data)
             dbConn = sql.connect(self.dbName)
             try:
                 inDataDf.to_sql(location, dbConn, if_exists='append', index = False)
@@ -39,23 +41,46 @@ class Access:
             dbConn.close()
         else:
             print("item already exists")
-    def reserve(self, barcode, location):
+    def removeItem(self, barcode, location):
+        self._extract(location)
+        if not self._dupCheck(barcode):
+            print("Item not in inventory")
+            return
         self._extract(location)
         data = self.dataDf.to_dict()
         barcodes = data["barcode"]
         barcodes_val = list(barcodes.values())
-        try:
-            position = barcodes_val.index(barcode)
-        except ValueError as error:
+        position = barcodes_val.index(barcode)
+        for item, itemdata in data.items():
+            itemdata.pop(position)
+            data[item] = itemdata
+        self._update(data, location)
+    def reserve(self, barcode, location):
+        if not self._dupCheck(barcode):
+            print("Item does not exist")
             return
-        print(data["reserved"][position])
+        self._extract(location)
+        data = self.dataDf.to_dict()
+        barcodes = data["barcode"]
+        barcodes_val = list(barcodes.values())
+        position = barcodes_val.index(barcode)
         if data["reserved"][position] == "false":
-            data["reserved"][position] = "true"
+            reserveId = input("reserve ID: ")
+            data["reserved"][position] = reserveId
             self._update(data, location)
-        elif data["reserved"][position] == "true":
-            print("Item " + barcode + " is reserved")
         else:
-            print("error")
+            print("Item " + barcode + " is reserved")
+    def unreserve(self, barcode, location):
+        if not self._dupCheck(barcode):
+            print("Item does not exist")
+            return
+        self._extract(location)
+        data = self.dataDf.to_dict()
+        barcodes = data["barcode"]
+        barcodes_val = list(barcodes.values())
+        position = barcodes_val.index(barcode)
+        data["reserved"][position] = "false"
+        self._update(data, location)
     def _update(self, newData, location):
         newDataDf = pd.DataFrame(newData)
         dbConn = sql.connect(self.dbName)
@@ -64,28 +89,35 @@ class Access:
     def printDf(self, location):
         self._extract(location)
         print(self.dataDf)
-        
-if __name__ == "__main__":
-    controls = ["add", "reserve"]
+
+def Run():
+    controls = ["quit", "show", "add", "remove", "reserve", "unreserve"]
     database = Access("someDB.db")
-    location = "e"
+    location = "db"
     while True:
         control = "false"
         barIn = "false"
         while not control in controls:
             control = input("enter control: ")
-        while (not barIn.isnumeric()) or (len(barIn) != barCodeLength):
-            barIn = input("enter barcode: ")
+        if control == controls[0]:
+            break
+        elif control == controls[1]:
+            database.printDf(location)
+        else:
+            while (not barIn.isnumeric()) or (len(barIn) != barCodeLength):
+                barIn = input("enter barcode: ")
+                if barIn == "-1":
+                    break
             if barIn == "-1":
                 break
-        if barIn == "-1":
-            break
-        if control == controls[0]:
-            data = {"barcode" : [barIn],
-                    "price" : [10],
-                    "reserved": ["false"]}
-            database.store(data, location)
-        elif control == controls[1]:
-            database.reserve(barIn, location)
-        
-        database.printDf(location)
+            if control == controls[2]:
+                database.store(barIn, location)
+            elif control == controls[3]:
+                database.removeItem(barIn, location)
+            elif control == controls[4]:
+                database.reserve(barIn, location)
+            elif control == controls[5]:
+                database.unreserve(barIn, location)
+                
+if __name__ == "__main__":
+    Run()
